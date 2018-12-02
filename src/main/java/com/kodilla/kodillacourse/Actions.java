ba54a931ad8b383d9d87f8bf0c4addfdee1f5d30
BULWAHR <BULWAHR>
2018-12-02 14:11:00 +0100

    blad

---
+++ src/main/java/com/kodilla/kodillacourse/Actions.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillacourse;
+
+public class Actions {
+
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public int substractNumbers(int a, int b) {
+        return a - b;
+    }
+}
--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -1,10 +1,12 @@
 package com.kodilla.kodillacourse;
 
 public class Calculator {
+    public static void main (String[] args) throws java.lang.Exception {
 
-    public static void main(String[] args) {
+        Actions FirstAction = new Actions();
+        Actions SecondAction = new Actions();
 
-
-
+            System.out.println("Adding result is: " + FirstAction.addNumbers(7,3));
+            System.out.println("Susbtraction result is: " + SecondAction.substractNumbers(5,2));
     }
 }
