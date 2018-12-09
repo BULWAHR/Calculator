fcd6102239f5ea5e5a76af6e497a949b8e6c4e8b
BULWAHR <BULWAHR>
2018-12-09 21:14:35 +0100

    update

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/Shape.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.shape;
+
+public interface Shape {
+
+    public String getShapeName() {
+        return cos;
+    }
+
+    public double getField() {
+        return 100;
+    }
+}
