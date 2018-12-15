4000afc5b994a14a966689f141bc71b4faef565b
BULWAHR <BULWAHR>
2018-12-15 12:16:16 +0100

    intelliJ

--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -4,6 +4,8 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.*;
+import java.io.*;
+
 
 
 public class ShapeCollectorTestSuite {
@@ -59,19 +61,28 @@ public class ShapeCollectorTestSuite {
     @Test
     public void testShowFigures() {
         ShapeCollector shapeCollector = new ShapeCollector();
-        // stworzyłem 3 obiekty
-        Shape square1 = new Square(3.5);
-        Shape triangle1 = new Triangle(2.5, 6.1);
-        Shape circle1 = new Circle(1.6);
 
-        // umieściłem je w liście
-        shapeCollector.addFigure(square1);
-        shapeCollector.addFigure(triangle1);
-        shapeCollector.addFigure(circle1);
+        String square1 = "Square(3.5)";
+        String triangle1 = "Triangle(2.5, 6.1)";
+        String circle1 = "Circle(1.6)";
 
-        // nie bardzo wiem jak dokończyć ten test
-        // chodzi o test takiej samej ilości obiektów?
-        // czy test sprawdzający czy są takie same figury?
+        String expectedListOfFigures = String.format("%s\n%s\n%s\n", square1, triangle1, circle1);
+
+        Shape square2 = new Square(3.5);
+        Shape triangle2 = new Triangle(2.5, 6.1);
+        Shape circle2 = new Circle(1.6);
+
+        shapeCollector.addFigure(square2);
+        shapeCollector.addFigure(triangle2);
+        shapeCollector.addFigure(circle2);
+
+        ByteArrayOutputStream temporaryString = new ByteArrayOutputStream();
+        PrintStream temporaryList = new PrintStream(temporaryString);
+
+        shapeCollector.showFigures(temporaryList);
+        String actualListOfFigures = PrintStream.toString();
+
+        Assert.assertEquals(expectedListOfFigures, actualListOfFigures);
     }
 }
 
